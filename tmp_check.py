e7abc880311d021e10d0b749f6f8125b221d20dd
HamedKazemii <mr.hamedkazemi@gmail.com>
2026-08-11 11:32:26 +0000

    tmp

--- tmp_check.py
+++ tmp_check.py
@@ -1,11 +1,8 @@
 import sys, json
 data = json.load(sys.stdin)
-for item in data:
-    if item.get("l18") == "عیار":
-        print("m_board:", item.get("m_board"))
-        print("m_board_id:", item.get("m_board_id"))
-        print("cs:", item.get("cs"))
-        print("cs_id:", item.get("cs_id"))
-        print("cs_sub:", item.get("cs_sub"))
-        print("cs_sub_id:", item.get("cs_sub_id"))
-        break
+print("m_board:", data.get("m_board"))
+print("m_board_id:", data.get("m_board_id"))
+print("cs:", data.get("cs"))
+print("cs_id:", data.get("cs_id"))
+print("cs_sub:", data.get("cs_sub"))
+print("cs_sub_id:", data.get("cs_sub_id"))
